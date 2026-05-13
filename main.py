958dfe9cfc1e0388537547cacfe39559188ae968
lijiam-boop <lijiam@wsc.school.nz>
2026-05-14 10:28:36 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-print("hello")
+# print("hello")
 
-print("5"+"5")
-print(5+5)
-print(5.0+5.0)
-print("5.0"+"5.0")
-print("5"+"5.0")
-print(5.0+5)
-print("5.0"+5)
+# print("5"+"5")
+# print(5+5)
+# print(5.0+5.0)
+# print("5.0"+"5.0")
+# print("5"+"5.0")
+# print(5.0+5)
+# print("5.0"+str(5))
+
+
+# print(5+5==10)
+# print(5+5==8)
+# print(5==5.0)
+# print('hello'=='hi')
+# print(5+5>=10)
+# print(5+5>=8)
+# print(5!=5)
+# print(5!=6)
+# print('hello'!='hi')
+# print(5=='5')
+# print(5)
+
+# if 5 < 10
+# if 5 < 10:
+# print('hello')
+# if 5 < 10:
+#    print('hello')
+# if 5 < 10:
+#    pass
+if 10 != 9:
+   print('hello')
+print('goodbye')
+
+
