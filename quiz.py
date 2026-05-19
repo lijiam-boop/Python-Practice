90080c805d50eed842b7a49101e8da626d7dcf7d
lijiam-boop <lijiam@wsc.school.nz>
2026-05-19 15:07:15 +1200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -4,10 +4,19 @@ print(" /\ /\\")
 print("((ovo))")
 print("():::()")
 print("  VVV")
-q1 = input("1. What is your favorite bird?")
-print("Great choice! I like "+q1+"s too!")
-q2=input("Do you have yourself bird?")
-q3 = input("2. Why do you like that bird?")
-q4 = input("3. What is your bird's favorite food?")
-q5= input("4. What do most birds use to fly? ")
-print("Thanks for taking the quiz! I hope you enjoyed it!")
+# q1 = input("1. What is your favorite bird?")
+# print("Great choice! I like "+q1+"s too!")
+# q2=input("Do you have yourself bird?")
+# q3 = input("2. Why do you like that bird?")
+# q4 = input("3. What is your bird's favorite food?")
+# q5= input("4. What do most birds use to fly? ")
+# print("Thanks for taking the quiz! I hope you enjoyed it!")
+
+q1 = input("Do you believe the Earth is round? ")
+if q1 == "Yes":
+    print("Correct! The Earth is round.")
+elif q1 == "No":
+    print("Incorrect. The Earth is round.")
+
+else:
+    print("Incorrect.Please try again")
