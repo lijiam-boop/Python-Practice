89548690263cf3610afd235ec05a3ae0a5848557
lijiam-boop <lijiam@wsc.school.nz>
2026-06-02 14:57:00 +1200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,28 +1,28 @@
 print("Hello")
 print("Welcome to my quiz about birds!")
-print(" /\ /\\")
+print(" /\\/\\")
 print("((ovo))")
 print("():::()")
 print("  VVV")
-# q1 = input("1. What is your favorite bird?")
-# print("Great choice! I like "+q1+"s too!")
-# q2=input("Do you have yourself bird?")
-# q3 = input("2. Why do you like that bird?")
-# q4 = input("3. What is your bird's favorite food?")
-# q5= input("4. What do most birds use to fly? ")
-# print("Thanks for taking the quiz! I hope you enjoyed it!")
+q1 = input("1. What is your favorite bird? ")
+print("Great choice! I like {q1}'s too!")
+q2 = input("Do you have yourself bird? ")
+q3 = input("2. Why do you like that bird? ")
+q4 = input("3. What is your bird's favorite food?")
+q5 = input("4. What do most birds use to fly? ")
+print("Thanks for taking the quiz! I hope you enjoyed it!\n")
 
-#q1 = input("Do you believe the Earth is round? A. Yes B. No C. Not sure ")
-#if q1 == "Yes":
- #   print("Correct! The Earth is round.")
-#elif q1 == "yes":
-  #  print("Correct! The Earth is round.")
-#elif q1 == "A":
- #   print("Correct! The Earth is round.")
-#elif q1 == "B":
-  #  print("Incorrect! The Earth is round.")
-#else:
-    #print("Incorrect. The Earth is round.")
+q1 = input("Do you believe the Earth is round? A. Yes B. No C. Not sure ")
+if q1 == "Yes":
+ print("Correct! The Earth is round.")
+elif q1 == "yes":
+ print("Correct! The Earth is round.")
+elif q1 == "A":
+ print("Correct! The Earth is round.")
+elif q1 == "B":
+ print("Incorrect! The Earth is round.")
+else:
+ print("Incorrect. The Earth is round.")
     
 q2 = input("What is the largest planet in our solar system? A. Earth B. Jupiter C. Mars ")
 if q2 == "B":
