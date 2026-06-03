93229e3ab5a4ebc70c443db446cec13ad1f55865
lijiam-boop <lijiam@wsc.school.nz>
2026-06-04 10:29:28 +1200

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,10 @@
 #    print('hello')
 # if 5 < 10:
 #    pass
-if 10 != 9:
-   print('hello')
-print('goodbye')
-
+guess = input("What’s the password?")
+print('Checking password is a match…')
+while guess != 'secret':
+     guess = input('Try again')
+     print('Checking password is a match…')
+input('Welcome!')
 
